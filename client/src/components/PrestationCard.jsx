85790a5bbf23bc76851ee9d17b4f431cc4ae372d
samih ringuet <samrin2@proton.me>
2024-07-25 18:50:22 +0200

    fix import google sheet

--- client/src/components/PrestationCard.jsx
+++ client/src/components/PrestationCard.jsx
@@ -1,14 +1,16 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/PrestationCard.css';
 
-const PrestationCard = ({ nom, description, prix, epoque, image }) => {
+const PrestationCard = ({ nom, description, prix, devise, epoque, image }) => {
   return (
     <div className="prestation-card">
-      <img src={image} alt={`${nom}`} className="prestation-image"/>
-      <h2>{nom}</h2>
+      <img src={image} alt={nom} className="prestation-image" />
+      <h3>{nom}</h3>
       <p>{description}</p>
       <p>Époque: {epoque}</p>
-      <p>Prix: {prix} €</p>
+      <p>Prix: {prix} {devise}</p>
+      
     </div>
   );
 };
@@ -19,9 +21,11 @@ PrestationCard.propTypes = {
   prix: PropTypes.string.isRequired,
   epoque: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  devise: PropTypes.string.isRequired,
 };
 
 export default PrestationCard;
+
 
 
 
